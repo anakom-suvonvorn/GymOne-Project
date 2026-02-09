57d84283877daaaa52472cf775f32168733adad1
anakom978 <anakom.suvonvorn@gmail.com>
2026-02-09 20:00:12 +0700

    with api implementation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from datetime import datetime, date, time, timedelta
+from fastapi import FastAPI, HTTPException
+import uvicorn
 
 class TimeSlot:
     def __init__(self, start, end, date, days_between=7, times=1): #-1>forever, other>number of times
@@ -63,6 +65,9 @@ class DateBookings:
     @property
     def booking_list(self):
         return self.__booking_list
+    
+    def add_booking(self, booking):
+        self.__booking_list.append(booking)
 
 class ClassSchedule(Schedule):
     def __init__(self, name, max_participants, timeslot):
@@ -105,8 +110,8 @@ class ClassSchedule(Schedule):
         
         new_booking = Booking(member.citizen_id, self.schedule_id, enroll_date)
 
-        date_booking_obj.booking_list.append(new_booking) 
-        member.booking_list.append(new_booking)
+        date_booking_obj.add_booking(new_booking) 
+        member.add_booking(new_booking)
 
         print(f"Booking successful:\n{new_booking}")
 
@@ -120,6 +125,15 @@ class ClassSchedule(Schedule):
         return new_db
 
     # needs to display to current enrolled / available participants slot
+
+    @property
+    def info(self):
+        return {
+            "name": self.name,
+            "schedule id": self.schedule_id,
+            "timeslot": f"{self.timeslot}",
+            "max participants": self.__max_participants
+        }
 
     def __str__(self):
         return f"[{self.name} | schedule id: {self.schedule_id}] {self.timeslot} | Max participants: {self.__max_participants}"
@@ -145,6 +159,18 @@ class GymClass:
         class_schedule = ClassSchedule(name, max_participants, timeslot)
         self.__class_schedule_list.append(class_schedule)
         return class_schedule
+    
+    @property
+    def info(self):
+        schedules = []
+        for class_schedule in self.__class_schedule_list:
+            schedules.append(class_schedule.info)
+
+        return {
+            "Class name": self.__name,
+            "Class id": self.__class_id,
+            "Class schedule": schedules
+        }
     
     def __str__(self):
         text = f"Class name: {self.__name}\nClass id: {self.__class_id}\nClass schedule:\n"
@@ -173,8 +199,11 @@ class Gym:
         self.__user_list.append(member)
         return member
     
-    # def get_available_classes(self):
-    #     for gym_class in self.__gym_class_list:
+    def get_available_classes(self):
+        class_list = []
+        for gym_class in self.__gym_class_list:
+            class_list.append(gym_class.info)
+        return class_list
 
     def print_available_classes(self):
         for gym_class in self.__gym_class_list:
@@ -210,7 +239,16 @@ class User:
 class Member(User):
     def __init__(self, citizen_id, name, age): #MEM-2023-001
         super().__init__(citizen_id, name, age)
-        self.booking_list = []
+        self.__booking_list = []
+
+    def add_booking(self, booking):
+        self.__booking_list.append(booking)
+
+    def get_current_bookings(self):
+        bookings = []
+        for booking in self.__booking_list:
+            bookings.append(booking.info)
+        return bookings
 
 class Booking:
     __next_id = 1
@@ -223,6 +261,20 @@ class Booking:
         self.__class_date = class_date
         self.__status = "Pending" # statuses: Waitlist Confirmed Check-in Completed Cancelled
 
+    @property
+    def info(self):
+        if self.__status == "Pending":
+            status_text = "Pending. Please Pay to Confirm Booking"
+        else:
+            status_text = self.__status
+        return {
+            "Booking id" : self.__booking_id,
+            "schedule id" : self.__schedule_id,
+            "Citizen id" : self.__citizen_id,
+            "Class date": self.__class_date,
+            "Status": status_text
+        }
+
     def __str__(self):
         if self.__status == "Pending":
             status_text = "Pending. Please Pay to Confirm Booking"
@@ -230,8 +282,8 @@ class Booking:
             status_text = self.__status
         text = f"Booking id: {self.__booking_id}\nschedule id: {self.__schedule_id}\nCitizen id: {self.__citizen_id}\nClass date: {self.__class_date}\nStatus: {status_text}"
         return text
-
-def run_test():
+    
+def create_stuff():
     gym = Gym("my gym", "1/45 bangkok thailand")
     
     yoga_class = gym.create_class("yoga")
@@ -248,6 +300,9 @@ def run_test():
 
     bob_membership = gym.create_member("123456789", "Bobda builder", 19)
 
+    return gym, bob_membership
+
+def run_test(gym, bob_membership):
     gym.print_available_classes() # to see and choose what to enroll in
 
     schedule_id = int(input("Enter schedule id to enroll bob into: "))
@@ -256,18 +311,56 @@ def run_test():
 
     gym.enroll_member(bob_membership, schedule_id, enroll_date)
 
-    # testing if check date logic is correct
-    # schedule = gym.get_schedule_by_id(2)
-    # for i in range(1,29):
-    #     print(f"{i} : {schedule.is_occuring(date(2026,2,i))}")
-    # for i in range(1,20):
-    #     print(f"{i} : {schedule.is_occuring(date(2026,3,i))}")
-    # print(schedule)
+    def minimize_to_cleaner():
+        # testing if check date logic is correct
+        # schedule = gym.get_schedule_by_id(2)
+        # for i in range(1,29):
+        #     print(f"{i} : {schedule.is_occuring(date(2026,2,i))}")
+        # for i in range(1,20):
+        #     print(f"{i} : {schedule.is_occuring(date(2026,3,i))}")
+        # print(schedule)
 
-    # after enrolling need to confirm by paying
-    # can cancel
+        # after enrolling need to confirm by paying
+        # can cancel
+        pass
+
+def run_api_test(gym: Gym, bob_membership: Member):
+    app = FastAPI()
+
+    @app.get("/")
+    def home():
+        return {"status": "Server is up!"}
+    
+    @app.get("/showclass")
+    def show_available_classes():
+        classes = gym.get_available_classes()
+        return {
+            "classes": classes,
+            "tip": "To enroll in class go to /enrollclass/{schedule_id} with the content {date: 'yyyy-mm-dd'}"
+        }
+    
+    @app.get("/showbooking")
+    def show_current_bookings():
+        bookings = bob_membership.get_current_bookings()
+        return {
+            "bookings": bookings,
+        }
+    
+    @app.post("/enrollclass/{schedule_id}")
+    def enroll_class(schedule_id: int, body: dict) -> dict:
+        try:
+            date_string = body["date"]
+            enroll_date = datetime.strptime(date_string, '%Y-%m-%d').date()
+            gym.enroll_member(bob_membership, schedule_id, enroll_date)
+            return {"success": f"bob has been succesfully enrolled into class with schedule_id: {schedule_id} on {enroll_date}"}
+        except Exception as e:
+            raise HTTPException(status_code=400, detail=str(e))
+
+    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
 
 if __name__ == "__main__":
-    run_test()
+    gym, bob_membership = create_stuff()
+    # run_test(gym, bob_membership)
+    run_api_test(gym, bob_membership)
 
 
